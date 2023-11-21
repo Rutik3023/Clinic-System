dceadb8ac23ca8ab28191eda076c84bb41fc1dc6
Rutik3023 <rutikthorat3023@gmail.com>
2023-11-21 11:12:16 +0530

    Rutik Add Class CasePaper

---
+++ ClinicManagementSystem/Servises/CasePaperRepo.cs
@@ -0,0 +1,12 @@
+﻿using ClinicManagementSystem.Iservises;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicManagementSystem.Servises
+{
+    public class CasePaperRepo:ICasePaper
+    {
+    }
+}
